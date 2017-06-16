e206fd3148d25bbf0f47eaeb97ba2bb2f4796791
anirudh <anirudh.chhangani@gmail.com>
2017-06-17 03:27:14 +0530

    remove map
    update deploy script

---
+++ src/app/views/college.jsx
@@ -0,0 +1,13 @@
+var m = require("mithril")
+var CollegeModel = require("../models/college.js")
+
+
+var CollegeView = {
+    view: function(){
+        return (
+            <div>Under construction</div>
+        )
+    }
+}
+
+module.exports = CollegeView
