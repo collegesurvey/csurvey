942e6a57c99dae054ae9599d20429f0a50c27492
anirudh <anirudh.chhangani@gmail.com>
2017-08-27 01:53:35 +0530

    css fixes

--- src/app/pages/account/view.jsx
+++ src/app/pages/account/view.jsx
@@ -6,18 +6,18 @@ var Account = {
     },
     view: function(vnode) {
         return (
-            <div class="btn-group">
-            <button class="btn btn-info dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                {vnode.attrs.name}
-                <span class="caret"></span>
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                <li><a href="#!/account/dash">Dashboard</a></li>
-                <li><a href="#!/account/profile">Profile</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="#!/account/logout">Logout</a></li>
-            </ul>
-            </div>
+            <li class="dropdown">
+                <a href="#!" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    {vnode.attrs.name}
+                    <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="#!/account/dash">Dashboard</a></li>
+                    <li><a href="#!/account/profile">Profile</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#!/account/logout">Logout</a></li>
+                </ul>
+            </li>
         )
     }
 }
--- src/layout.jsx
+++ src/layout.jsx
@@ -6,19 +6,19 @@ var Layout = {
 	view: function(vnode){
 		return (            
 			<main class="layout">
-				<nav class="navbar navbar-inverse navbar-fixed-top">
-					<div>
+				<nav class="navbar navbar-default navbar-fixed-top">
+					<div class="container-fluid">
 						<div class="navbar-header">
 							<a href="/" class="navbar-brand" oncreate={m.route.link}>CollegeSurvey</a>
 						</div>
 						<div class="collapse navbar-collapse" id="navbar">
-							<ul class="nav navbar-nav">
+							<ul class="nav navbar-nav navbar-left">
 								<li>
 									<a href="/about" oncreate={m.route.link}>About</a>
 								</li>
-								<li>
-									<Account name="Guest"/>
-								</li>
+							</ul>
+							<ul class="nav navbar-nav navbar-right">
+								<Account name="Guest"/>
 							</ul>
 						</div>
 					</div>
